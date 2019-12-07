1ef9868a66cd09bb0c6f809e2f69ff650ae493d6
Rayzal Muhammad <rayzal.anwar@emveep.com>
2019-12-08 01:49:43 +0700

    change setting dirname on server js

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/icma'));
+// app.use(express.static(__dirname + '/dist/icma'));
+app.use(express.static(`${__dirname}/icma/dist/`));
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname+'/dist/icma/index.html'));
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname+'/dist/icma/index.html'));
+// });
+app.get('*', (req, res) => {
+    res.sendFile(`./icma/dist/index.html`);
 });
 
 app.listen(process.env.PORT || 8080);
